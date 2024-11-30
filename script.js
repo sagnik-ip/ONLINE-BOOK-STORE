1f9714c7de9b1a35777d794ca69e857c56ce5d2c
sagnik-ip <sagnikuna@gmail.com>
2024-11-30 15:06:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript for opening book pages animation
+document.querySelector('.book-menu').addEventListener('click', function() {
+    // Toggle the 'open' class to trigger the opening pages animation
+    this.classList.toggle('open');
+  });
